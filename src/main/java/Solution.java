02ee5e5e22742f56ed07633c80d0710323ba2f46
tvv1994 <48904772+tvv1994@users.noreply.github.com>
2019-03-27 13:52:10 +0300

    Update Solution.java

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,17 +2,15 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Properties;
-import java.util.function.Consumer;
 import java.sql.*;
 
 public class Solution {
-    public static final String DB_URL="jdbc:h2:./src/main/resources/stockExchange";
-    public static final String DB_Driver="org.h2.Driver";
-    public static final String createTable="create table IF NOT EXISTS USERS (name VARCHAR(50), age INT, isRabbit BOOLEAN, skills VARCHAR(150));";
+
+    public static final String DB_URL = "jdbc:h2:" + Solution.class.getClassLoader().getResource("stockExchange").getPath();
+    public static final String DB_DRIVER = "org.h2.Driver";
+    public static final String CREATE_TABLE = "create table IF NOT EXISTS USERS (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), age INT, isRabbit BOOLEAN, skills VARCHAR(150));";
 
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
@@ -23,7 +21,8 @@ public class Solution {
         // т.к. вызов происходит из статичного метода, в случае, если мы хотим получить
         // класслоадер у инстанциированного класса - мы можем вызвать просто this.getClass()
         URL resource = Solution.class.getClassLoader().getResource("input.json");
-        List<User> list = mapper.readValue(resource, new TypeReference<List<User>>(){});
+        List<User> list = mapper.readValue(resource, new TypeReference<List<User>>() {
+        });
 
         // самый простой пример лямбда функции - forEach над коллекцией,
         // если упростить описание лямбд, то можно свести все к тому,
@@ -33,26 +32,21 @@ public class Solution {
         list.forEach(user -> System.out.println(user.getName()));
         // вот так выглядит forEach без "синтаксического сахара" лямбда-выражений
         //list.forEach(new Consumer<User>() {
-            //@Override
-            //public void accept(User user) {
-                //System.out.println(user.getName());
-            //}
+        //@Override
+        //public void accept(User user) {
+        //System.out.println(user.getName());
+        //}
         //});
-        Class.forName(DB_Driver);
-        Connection connection=DriverManager.getConnection(DB_URL);
+        Class.forName(DB_DRIVER);
+        Connection connection = DriverManager.getConnection(DB_URL);
         //Таблица
         Statement stH2 = connection.createStatement();
-        stH2.execute(createTable);
-        //Данные
-        for(User e:list){
-            String insert = creatString(e);
-            stH2.execute(insert);
-        }
-    }
+        stH2.execute(CREATE_TABLE);
 
-    public static String creatString(User u){
-        String skill=String.join(", ", u.getSkills());
-        String s= String.format("insert into USERS (name, age, isRabbit, skills) values ('%s', %d, %b, '%s');", u.getName(), u.getAge(), u.isRabbit(), skill);
-        return s;
+        UserRepository userRepository = new UserRepository(stH2);
+        userRepository.saveListOfUsers(list);
+        userRepository.readUser(2);
+        userRepository.removeUser(10);
+
     }
 }
